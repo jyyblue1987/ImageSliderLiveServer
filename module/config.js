5432ae2fb533b6b69eec8a71d8c50ec24aa9496e
jyyblue <jyyblue1987@outlook.com>
2020-02-11 03:12:15 +0200

    [add] student, teacher html.

--- module/config.js
+++ module/config.js
@@ -5,9 +5,4 @@ exports.MYSQL_CONFIG = {
     database : 'ennovatech'
 };
 
-exports.FTP_USER = 'ftpuser';
-exports.FTP_PASS = 'cameraviewer';
-exports.FTP_DIR = 'D:\Work\Samuil_K_FTP\ftp_dir';
-exports.FTP_DEST_DIR = 'D:\Work\Samuil_K_FTP\ftp_dest_dir';
-exports.UPLOAD_DIR = 'D:\Work\Samuil_K_FTP\cameraviewer';
 
--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,37 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var config = require('../module/config');
+
 const fs = require('fs');
 var sprintf = require("sprintf-js").sprintf,
     vsprintf = require("sprintf-js").vsprintf;
-const { exec } = require('child_process');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Express' });
 });
 
+router.get('/student-a', function(req, res, next) {
+    fs.readFile('public/student.html', function(err, data) {                
+        var str = data.toString();
+   
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(str);
+        res.end();
+    });
+});
+
+
 router.post('/createcamera', function(req, res, next) {	
 	var camera_id = req.body.camera_id;
 
-	dir = config.FTP_DIR + '/' + camera_id;
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
-
-	var command = sprintf('sudo chown %s:%s %s', config.FTP_USER, config.FTP_USER, dir)
-    exec(command, (err, stdout, stderr) => {
-        if (err) {
-            //some err occurred
-			console.error(err);
-			var data = {'error' : err};
-      		res.send(data);
-        } else {
-            // the *entire* stdout and stderr (buffered)
-            console.log(`stdout: ${stdout}`);
-			console.log(`stderr: ${stderr}`);
-			
-			var data = {code : 200};
-			res.send(data);
-        }
-    });	
+    //some err occurred
+    // console.error(err);
+    // var data = {'error' : err};
+    // res.send(data);
+    var data = {code : 200};
+    res.send(data);
 });
 
 
