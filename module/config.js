a23c63a524bab1db7a21cc4edb6b0e37725d5f17
jyyblue <jyyblue1987@outlook.com>
2020-02-11 02:43:29 +0200

    [create] create project.

---
+++ module/config.js
@@ -0,0 +1,13 @@
+exports.MYSQL_CONFIG = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '', // Hotlync_2@16 : temp_pro
+    database : 'ennovatech'
+};
+
+exports.FTP_USER = 'ftpuser';
+exports.FTP_PASS = 'cameraviewer';
+exports.FTP_DIR = 'D:\Work\Samuil_K_FTP\ftp_dir';
+exports.FTP_DEST_DIR = 'D:\Work\Samuil_K_FTP\ftp_dest_dir';
+exports.UPLOAD_DIR = 'D:\Work\Samuil_K_FTP\cameraviewer';
+
